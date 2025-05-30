903c3d830636bef19d84dab472757b88385e426d
serii burduja <seriiburduja@gmail.com>
2025-05-30 22:34:29 +0300

    feat: disable after-start

--- scripts/after-start.sh
+++ scripts/after-start.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -x
 
-nginx_path="docker/nginx/default.conf"
+nginx_path="/var/www/html/docker/nginx/default.conf"
 
 if [ ! -e "$nginx_path" ]; then
   echo "Nginx configuration file not found at $nginx_path"
   exit 1
 fi
 
-# check line site_name and show line
 if grep -q "site_name" "$nginx_path"; then
   echo "site_name found in $nginx_path"
   grep "site_name" "$nginx_path"
