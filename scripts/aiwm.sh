3bd95a3b99dc05c65b020f9fcee935bf3cca9163
serii burduja <seriiburduja@gmail.com>
2025-05-30 23:38:40 +0300

    feat: aiwmsh sudo

--- scripts/aiwm.sh
+++ scripts/aiwm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo chown -R 33:33 wp-content/ai1wm-backups wp-content/plugins/all-in-one-wp-migration/storage
-sudo chmod -R 755 wp-content/ai1wm-backups wp-content/plugins/all-in-one-wp-migration/storage
+chown -R 33:33 wp-content/ai1wm-backups wp-content/plugins/all-in-one-wp-migration/storage
+chmod -R 755 wp-content/ai1wm-backups wp-content/plugins/all-in-one-wp-migration/storage
 
