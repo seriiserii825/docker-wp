a89cff28e9ed0562ba7157ede7fe0f4bff64128e
serii burduja <seriiburduja@gmail.com>
2025-05-31 11:12:36 +0300

    feat: entrypoint aiwm check

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
 
-chmod -R a+rwX /var/www/html/wp-content/ai1wm-backups
-chmod -R a+rwX /var/www/html/wp-content/plugins/all-in-one-wp-migration/storage
+# chmod -R a+rwX /var/www/html/wp-content/ai1wm-backups
+# chmod -R a+rwX /var/www/html/wp-content/plugins/all-in-one-wp-migration/storage
+
+if [ -d /var/www/html/wp-content/ai1wm-backups ]; then
+  chmod -R a+rwX /var/www/html/wp-content/ai1wm-backups
+fi
+
+if [ -d /var/www/html/wp-content/plugins/all-in-one-wp-migration/storage ]; then
+  chmod -R a+rwX /var/www/html/wp-content/plugins/all-in-one-wp-migration/storage
+fi
 
 exec "$@"
