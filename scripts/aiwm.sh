3b6238195e19e796915dbba90fe30c246595ab89
serii burduja <seriiburduja@gmail.com>
2025-05-31 10:28:32 +0300

    feat: removed unused scripts added init-wp-docker.sh

--- scripts/aiwm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-chown -R 33:33 wp-content/ai1wm-backups wp-content/plugins/all-in-one-wp-migration/storage
-chmod -R 755 wp-content/ai1wm-backups wp-content/plugins/all-in-one-wp-migration/storage
-
